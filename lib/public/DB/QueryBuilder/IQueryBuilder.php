acc34286659956a5f6873a92152903ee20d85cca
Louis Chemineau <louis@chmn.me>
2024-04-08 13:48:26 +0200

    feat(DB): Move retry logic to the QueryBuilder
    Signed-off-by: Louis Chemineau <louis@chmn.me>

--- lib/public/DB/QueryBuilder/IQueryBuilder.php
+++ lib/public/DB/QueryBuilder/IQueryBuilder.php
@@ -185,6 +185,13 @@ interface IQueryBuilder {
 	public function executeQuery(): IResult;
 
 	/**
+	 * @since 30.0.0
+	 *
+	 * @param int $retryLimit - Retry the query up to $retryLimit times in case of a RetryableException. Initially equal to 4.
+	 */
+	public function setRetryLimit(int $retryLimit): void;
+
+	/**
 	 * Execute insert, update and delete statements
 	 *
 	 * @return int the number of affected rows
